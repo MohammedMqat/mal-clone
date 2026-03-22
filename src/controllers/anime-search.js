5357895db7dcf994f3296b273149d2c123d083f8
MohammedMqat <mohammedmqat@gmail.com>
2026-03-22 22:25:02 +0000

    add validation

--- src/controllers/anime-search.js
+++ src/controllers/anime-search.js
@@ -1,5 +1,10 @@
 export const searchAnime = (req, res) => {
-  const { q } = req.query;
+  const { q = "" } = req.query;
+
+  if (q.length === 0) {
+    return res.status(400).json({ message: "Please search with a query" });
+  }
+
   fetch(`https://api.jikan.moe/v4/anime?q=${q}`)
     .then((response) => response.json())
     .then((data) => res.json(data));
